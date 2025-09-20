96577460a92f924cf209bdd13d60dddfc9063553
robert-isaac <robert_isaac@outlook.com>
2025-09-19 20:14:39 -0400

    grid update (visual funcionando)

--- life_game/grid.py
+++ life_game/grid.py
@@ -1,8 +1,8 @@
 import collections
-alive = "O"
-dead = "."
-#agrega una clase life_grid con funcionalidades __init__, evolve, as_string y __str
-class life_grid:
+ALIVE = "♥"
+DEAD = "."
+#agrega una clase LifeGrid con funcionalidades __init__, evolve, as_string y __str
+class LifeGrid:
     def __init__(self, pattern): #inicializa el grid con un patron dado
         self.pattern = pattern
     def evolve(self): #calcula la siguiente generacion
@@ -14,7 +14,7 @@ class life_grid:
         num_neighbors = collections.defaultdict(int)
         for row, col in self.pattern.alive_cells:
             for dr, dc in neighbors:
-                num_neighbors[row + dr, col + dc] +=1 
+                num_neighbors[row + dr, col + dc] += 1 
                  
         #condiciones de nacimiento y supervivencia
         stay_alive = {cell for cell in self.pattern.alive_cells if num_neighbors[cell] in (2, 3)}
@@ -22,16 +22,18 @@ class life_grid:
         self.pattern.alive_cells = stay_alive | come_alive
         
     def as_string(self,bbox): #devuelve una representacion en string del grid
-        start_row, start_col, end_row, end_col = bbox
-        display = [self.pattern.name.center(2*(end_col - start_col))]
+        start_col, start_row, end_col, end_row = bbox
+        display = [self.pattern.name.center(2 * (end_col - start_col))]
         for row in range(start_row, end_row):
-            display = [alive if (row,col) in self.pattern.alive_celss else dead
-                       for col in range(start_col, end_col)]
+            display_row = [
+            ALIVE if (row, col) in self.pattern.alive_cells else DEAD
+            for col in range(start_col, end_col)
+            ]
             display.append(" ".join(display_row))
-            return "\n ".join(display)
-
+        return "\n".join(display)
     def __str__(self): #devuelve una representacion en string del grid
-        return (
+       return (
             f"{self.pattern.name}:\n"
             f"Alive cells -> {sorted(self.pattern.alive_cells)}"
-        )     
+        )
+      
